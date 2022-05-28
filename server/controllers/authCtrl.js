b40b3751604560d0a92fc587b2c80620d075b95f
Noobieprogrammer69 <joververgara1@gmail.com>
2022-05-28 18:25:44 +0800

    make it better

--- server/controllers/authCtrl.js
+++ server/controllers/authCtrl.js
@@ -85,7 +85,7 @@ const authCtrl = {
 },
     logout: async ( req, res ) => {
         try {
-            res.clearCookie('refreshtoken', {path: "/api/refresh_token"})
+            res.clearCookie('refreshtoken', {path: '/api/refresh_token'})
             res.json({msg: "Logged out"})
         } catch (error) {
         return res.status(500).json({ msg: error.message })
